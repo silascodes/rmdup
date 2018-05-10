99095f6586f44e5f259d1a54b04bdddefe080c5b
silascodes <silas.normanton@gmail.com>
2018-05-10 20:56:42 +1200

    Converted multiline style comments to single line style comments Corrected hash algorithm reference in a comment

--- src/rmdup.c
+++ src/rmdup.c
@@ -1,25 +1,22 @@
-/* rmdup.c -- duplicate file removal tool
-  version beta-2, October 7th, 2012
+// rmdup.c -- duplicate file removal tool
+// version beta-2, October 7th, 2012
 
-  Silas Normanton
-  silas.normanton@gmail.com
+// Silas Normanton
+// silas.normanton@gmail.com
 
-*/
-
-/* PORTING NOTES
- *	Theres minimal Win32 specific code for scanning files and directories, everything else should be fine.
- *	Search WIN32 to find the Win32 specific code.
- */
+// PORTING NOTES
+//	Theres minimal Win32 specific code for scanning files and directories, everything else should be fine.
+//	Search WIN32 to find the Win32 specific code.
 
 #include <stdio.h>
 #include <stdarg.h>
-#include <windows.h>	/* WIN32 specific */
+#include <windows.h>	// WIN32 specific
 
 #include "md5.h"
 
 #define RMDUP_VERSION		"beta-2"
 #define RMDUP_MAXPATH		1024
-#define RMDUP_BUFSIZE		2048        /* After some profiling this seemed most optimal, on my AMD FX box hitting RAID0 SATA3 drives */
+#define RMDUP_BUFSIZE		2048        // After some profiling this seemed most optimal, on my AMD FX box hitting RAID0 SATA3 drives
 
 #define RMDUP_FLAG_RECURSE	(1 << 0)
 #define RMDUP_FLAG_SAFE		(1 << 1)
@@ -144,35 +141,35 @@ void freelist() {
 	}
 }
 
-/* WIN32 specific stuff in here, enjoy... */
+// WIN32 specific stuff in here, enjoy...
 void rmdup(char *path) {
 	HANDLE h;
 	WIN32_FIND_DATA findData;
 	char findPath[RMDUP_MAXPATH], dirPath[RMDUP_MAXPATH];
 	unsigned char md5[16];
 
-	/* Make working copy of path, append \* */
+	// Make working copy of path, append \*
 	memcpy(findPath, path, RMDUP_MAXPATH);
 	strncat(findPath, "\\*", 2);
 
 	rmdup_print("Processing directory %s\n", path);
 	dirsProcessed += 1;
 
-	/* Process all entries in the directory specified in path */
-	h = FindFirstFileEx(findPath, FindExInfoStandard, &findData, FindExSearchNameMatch, NULL, 0);/*FIND_FIRST_EX_LARGE_FETCH);*/
+	// Process all entries in the directory specified in path
+	h = FindFirstFileEx(findPath, FindExInfoStandard, &findData, FindExSearchNameMatch, NULL, 0);//FIND_FIRST_EX_LARGE_FETCH);
 	if(h == INVALID_HANDLE_VALUE) {
 		rmdup_print("Error: failed to scan path %s\n", path);
 		return;
 	}
 	do {
 		if(findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
-			/* Process directories, recursing if we need to */
+			// Process directories, recursing if we need to
 			if(strcmp(findData.cFileName, ".") == 0 || strcmp(findData.cFileName, "..") == 0) {
-				/* Ignore . (current) and .. (1 level up) */
+				// Ignore . (current) and .. (1 level up)
 				continue;
 			}
 			if(flags & RMDUP_FLAG_RECURSE) {
-				/* Prepare the new path then recurse to it */
+				// Prepare the new path then recurse to it
 				memcpy(dirPath, path, RMDUP_MAXPATH);
 				strncat(dirPath, "\\", 1);
 				strcat(dirPath, findData.cFileName);
@@ -181,14 +178,14 @@ void rmdup(char *path) {
 			}
 		}
 		else if(!(findData.dwFileAttributes & FILE_ATTRIBUTE_HIDDEN) && !(findData.dwFileAttributes & FILE_ATTRIBUTE_SYSTEM)) {
-			/* Get the full absolute path to the file */
+			// Get the full absolute path to the file
 			memcpy(dirPath, path, RMDUP_MAXPATH);
 			strncat(dirPath, "\\", 1);
 			strcat(dirPath, findData.cFileName);
 
 			filesProcessed += 1;
 
-			/* Calculate the CRC32 checksum for the file, if it's in our list delete the file, otherwise add the CRC to our list */
+			// Calculate the MD5 checksum for the file, if it's in our list delete the file, otherwise add the MD5 to our list
 			md5file(dirPath, md5);
 			if(containsmd5(md5) == 0) {
 				addmd5(md5);
@@ -217,7 +214,7 @@ int main(int argc, char **argv) {
 	else {
 		memset(path, 0, RMDUP_MAXPATH);
 
-		/* Process command line */
+		// Process command line
 		for(i = 1; i < argc; i++) {
 			if(argv[i][0] == '-') {
 				for(j = 1; j < strlen(argv[i]); j++) {
@@ -243,27 +240,27 @@ int main(int argc, char **argv) {
 
 		title();
 
-		/* Check we actually got a path */
+		// Check we actually got a path
 		i = strlen(path);
 		if(strlen(path) == 0) {
 			rmdup_print("Error: no path specified\n");
 			return -1;
 		}
 
-		/* Strip trailing slashes from path */
+		// Strip trailing slashes from path
 		i -= 1;
 		if(path[i] == '\\' || path[i] == '/') {
 			path[i] = 0;
 		}
 
-		/* Validate path */
-		i = GetFileAttributes(path); /* WIN32 specific */
+		// Validate path
+		i = GetFileAttributes(path); // WIN32 specific
 		if(i == INVALID_FILE_ATTRIBUTES || !(i & FILE_ATTRIBUTE_DIRECTORY)) {
 			rmdup_print("Error: specified path is not a valid directory\n");
 			return -1;
 		}
 
-		/* Process the specified directory */
+		// Process the specified directory
 		rmdup(path);
 
 		results();
